2da21b94c2da136e25aa9c4279ce831bb4afa55d
GavinChenYiwei <gavin.yiwei@gmail.com>
2019-01-07 12:33:43 -0800

    function & another way select element

--- Pig Games/app.js
+++ Pig Games/app.js
@@ -15,19 +15,44 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6 +1);
-
 //first way: change elements in webpage
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice  + '<em>'
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+//another way to select element
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+
+//function btn(){
+//	// DO sth here
+//}
+
+//call back function
+//document.querySelector('.btn-roll').addEventListener('click', btn)
+
+
+//anaoymous function, cannot reuse
+document.querySelector('.btn-roll').addEventListener('click', function(){
+	
+	//1. Random number
+	var dice = Math.floor(Math.random() * 6 +1);
+	
+	//Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	
+	// Upate the round score if the rolled number was not 1
+})
 
 
 
