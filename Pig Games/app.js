7b7f66436e8c1b9fc25fceaffec90e5f82c0325b
GavinChenYiwei <gavin.yiwei@gmail.com>
2019-01-08 11:53:45 -0800

    add hold function and remove duplicate lines

--- Pig Games/app.js
+++ Pig Games/app.js
@@ -58,7 +58,35 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	}else{
 		//Next player
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		nextPlayer();
+	}
+	
+})
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	
+	// add current score to the global score
+	scores[activePlayer] +=roundScore;
+	
+	//update the ui
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+	
+	
+	//check the player if win the game
+	if(scores[activePlayer] >= 10){
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else{
+		//next Player
+		nextPlayer();
+	}
+		
+})
+
+function nextPlayer(){
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 		roundScore = 0;
 		
 		document.getElementById('current-0').textContent = '0';
@@ -72,9 +100,4 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		document.querySelector('.player-1-panel').classList.toggle('active');
 		
 		document.querySelector('.dice').style.display = 'none';
-	}
-	
-})
-
-
-
+}
